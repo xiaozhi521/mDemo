d718d7306a87cbb50d4a4d3cbf5bb4b016d83c2b
li_hongzhi <1160948933@qq.com>
2023-02-27 14:39:27 +0800

    first

---
+++ src/main/java/com/example/mdemo/MDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.mdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mdemo/MDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
